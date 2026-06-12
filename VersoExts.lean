a13e1da6ac84bc3a0b9083afdb0e56fe9ea73514
Richard L Ford <richardlford@gmail.com>
2026-06-12 17:07:41 -0400

    Add srclink role

--- VersoExts.lean
+++ VersoExts.lean
@@ -8,7 +8,7 @@ section Slink
 
 variable [Monad m] [MonadInfoTree m] [MonadLiftT CoreM m] [MonadEnv m] [MonadError m]
 
-/-- And edit link is a link which opens an editor, specifically vscode,
+/-- An edit link is a link which opens an editor, specifically vscode,
     to the specified path. -/
 structure EditLink where
   path : String
@@ -16,7 +16,7 @@ structure EditLink where
 instance : FromArgs EditLink m where
   fromArgs := EditLink.mk  <$> positional' `path
 
-/-- Define an `editlink` role. It expands to a link which opens an editor, specifically vscode,
+/-- Define the `editlink` role. It expands to a link which opens an editor, specifically vscode,
     to the specified path. If the path is relative, it is resolved with respect to the current working directory at the time the verso project is built. If the absolute path does not exist or is not a file, an error is thrown.
 -/
 @[role]
@@ -29,5 +29,33 @@ def editlink : RoleExpanderOf EditLink
     if ! isFile then
       throwError s!"{path} interpreted as {absPath} either does not exist or is not a file."
     ``(Inline.link #[$[$(← args.mapM elabInline)],*] $(quote <| "vscode:" ++ s!"{absPath}" ))
+
+/-- A src link is a link which opens an and editor, specifically vscode,
+    to the specified path, using an environment variable to resolve
+    relative paths. -/
+structure SrcLink where
+  path : String
+
+instance : FromArgs SrcLink m where
+  fromArgs := SrcLink.mk  <$> positional' `path
+
+/-- Define the `srclink` role. It expands to a link which opens an editor, specifically vscode,
+    to the specified path. If the path is relative, it is resolved with respect to the SOURCE_ROOT
+    at the time the verso project is built. If the absolute path does not exist or is not a file, an error is thrown.
+-/
+@[role]
+def srclink : RoleExpanderOf SrcLink
+  | { path }, args => do
+    let fsPath := System.FilePath.mk path
+    let root? ← IO.getEnv "SOURCE_ROOT"
+    let root ← match root? with
+      | some r => pure r
+      | none => throwError "SOURCE_ROOT environment variable is not set, cannot resolve relative paths for srclink role."
+    let absPath := if fsPath.isRelative then System.FilePath.mk (s!"{root}" ++ "/" ++ path) else fsPath
+    let isFile := (← absPath.pathExists) && ! (← absPath.isDir)
+    if ! isFile then
+      throwError s!"{path} interpreted as {absPath} either does not exist or is not a file."
+    ``(Inline.link #[$[$(← args.mapM elabInline)],*] $(quote <| "vscode:" ++ s!"{absPath}" ))
+
 
 end Slink
